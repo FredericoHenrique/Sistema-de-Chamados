12332273e40b005e4ed0d66d28c3d3eb69b94a3f
FredericoHenrique <69646055+FredericoHenrique@users.noreply.github.com>
2022-06-28 07:48:05 -0300

    Finalizando a abertura de chamado e adicionando imagens

---
+++ registra_chamado.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+// Montagem do chamado
+$titulo = str_replace('#', '-', $_POST['titulo']);
+$categoria = str_replace('#', '-', $_POST['categoria']);
+$descricao = str_replace('#', '-', $_POST['descricao']);
+
+// abertura do arquivo
+$arquivo = fopen('arquivo.txt', 'a'); 
+
+// difinindo a escrita
+$texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL; 
+
+// escrevendo no arquivo
+fwrite($arquivo, $texto); 
+
+// fechando o arquivo
+fclose($arquivo); 
+header('Location: abrir_chamado.php');
