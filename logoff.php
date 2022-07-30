fcab8afbc7dea2ca8053f948e484a7e99bd0d972
FredericoHenrique <69646055+FredericoHenrique@users.noreply.github.com>
2022-07-30 09:41:08 -0300

    .

--- logoff.php
+++ logoff.php
@@ -8,3 +8,26 @@ session_start();
 
 session_destroy();
 header('Location: index.php');
+
+
+class Mensagem {
+
+    private $para = null;
+    private $assunto = null;
+    private $mensagem = null;
+
+    public function __get($atributo)
+    {
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
+    }
+
+    public function mensagemValida()
+    {
+        //
+    }
+}
